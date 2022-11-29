cf9460a4db05042c71070a5416920395d3278377
AleksanderDmowski <aleksander.dmowski.work@gmail.com>
2022-11-29 05:47:25 +0100

    Update 2022-11-29 v0.2.0

--- GPW_scrapper.py
+++ GPW_scrapper.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup as BSop
 import time
 from datetime import datetime, timedelta
 import pandas as pd
-# version 0.1.0
+# version 0.2.0
 
 
 def get_data_from_gpw_link(date):
@@ -38,50 +38,91 @@ def create_values_list_from_data(numbers):
 
 def merge_lists(names, values):
     number_of_values = 8
-    merged_list = []
-    for postion in range(len(names)):
-        merged_list += [[names[postion], *
-                         values[(postion*number_of_values):((postion*number_of_values)+number_of_values)]]]
+    merged_list = [(([names[i]])+(values[i*number_of_values:(i*number_of_values)+number_of_values]))
+                   for i in range(len(names))]
     return merged_list
 
 
+def calculate_years_between(date1, date2):
+    try:
+        number_of_indywidual_years = int(
+            date2.strftime("%Y"))-int(date1.strftime("%Y"))
+        return number_of_indywidual_years+1
+    except AttributeError:
+        return 0
+
+
+def marge_csv_files(year, delate=False):
+    months = ['01', '02', '03', '04', '05',
+              '06', '07', '08', '09', '10', '11', '12']
+    csv_list = ['GPW_{}-{}.csv'.format(year, nr) for nr in months]
+    df_end = pd.concat(
+        map(pd.read_csv, csv_list), ignore_index=True)
+    df_end.to_csv(("{}{}{}").format('GPW_', year, '.csv'),
+                  index=False, encoding='utf-8')
+    print('{}{}{}{}'.format('Saved as: ', 'GPW_', year, '.csv'))
+    if delate:
+        print('Sub files delated')
+        # delate file
+
+
 def scrap_and_convert_into_csv():
-    staring_date = datetime(2022, 11, 21)
-    end_date = datetime(2022, 11, 21)
-    start_time = time.time()
-    date_to_loop = staring_date
-    range_of_dates = (end_date-staring_date).days
-    gpw_columns = [
-        'Nazwa spółki',
-        'Kurs otwarcia',
-        'Kurs min',
-        'Kurs max',
-        'Kurs zamknięcia',
-        'Zmiana kursu w procent',
-        'Wolumen obrotu w szt',
-        'Lista transkacji',
-        'Wartość obrotu w tys',
-        'Dzień']
-    df = pd.DataFrame(columns=gpw_columns)
-    row = pd.DataFrame(columns=gpw_columns)
-    for day in range(range_of_dates+1):
-        print(date_to_loop)
-        soup = BSop(get_data_from_gpw_link(date_to_loop).text, 'html.parser')
-        names = soup.select('.left')
-        values = soup.select('.text-right')
-        data_list_for_loop = merge_lists(create_names_list_from_data(
-            names), create_values_list_from_data(values))
-        if len(data_list_for_loop) != 0:
-            for record in data_list_for_loop:
-                record.append(("{}").format(date_to_loop))
-                row.loc[len(row)] = record
-                df = pd.concat([row], axis=0)
+    years_range = [2014]
+    # (2013,2012,2011,2010,2009,2008,2007,2006,2005,2004,2003,2002,2001,2000)
+    for year in years_range:
+        start_time = time.time()
+        staring_date = datetime(year, 1, 1)
+        end_date = datetime(year, 12, 31)
+        date_to_loop = staring_date
+        range_of_days = (end_date-staring_date).days
 
-        date_to_loop += timedelta(days=1)
+        gpw_columns = [
+            'Nazwa spółki',
+            'Kurs otwarcia',
+            'Kurs min',
+            'Kurs max',
+            'Kurs zamknięcia',
+            'Zmiana kursu w procent',
+            'Wolumen obrotu w szt',
+            'Lista transkacji',
+            'Wartość obrotu w tys',
+            'Dzień']
 
-    df.to_csv(("{}").format('custom_file_name.csv'),
-              header=df.columns, index=False, encoding='utf-8')
-    print(time.time()-start_time)
+        df = pd.DataFrame(columns=gpw_columns)
+        row = pd.DataFrame(columns=gpw_columns)
+        month = 1
+        start_time_for_month = time.time()
+        for day in range(range_of_days+1):
+            print('month: ', int(date_to_loop.strftime("%m")), month,
+                  ' /12 _ ', day+1, '/', range_of_days+1)
+            soup = BSop(get_data_from_gpw_link(
+                date_to_loop).text, 'html.parser')
+            names = soup.select('.left')
+            values = soup.select('.text-right')
+            data_list_for_loop = merge_lists(create_names_list_from_data(
+                names), create_values_list_from_data(values))
+            if len(data_list_for_loop) != 0:
+                for record in data_list_for_loop:
+                    record.append(("{}").format(date_to_loop))
+                    row.loc[len(row)] = record
+                    df = pd.concat([row], axis=0)
+
+            date_to_loop += timedelta(days=1)
+            if month != int(date_to_loop.strftime("%m")):
+                df.to_csv(("{}{}{}").format('GPW_', (date_to_loop-timedelta(days=1)).strftime("%Y-%m"), '.csv'),
+                          header=df.columns, index=False, encoding='utf-8')
+                df = df.iloc[0:0]
+                df = pd.DataFrame(columns=gpw_columns)
+                row = row.iloc[0:0]
+                row = pd.DataFrame(columns=gpw_columns)
+                print('Saved, this month: ', (time.time()-start_time_for_month))
+                month += 1
+                start_time_for_month = time.time()
+
+        marge_csv_files(year)
+        print('Total time spend for this year: ', time.time()-start_time)
 
 
-scrap_and_convert_into_csv()
+App_run = True
+if App_run:
+    scrap_and_convert_into_csv()
